26a2e3809956bf11f51edacc5d38e7fba07032aa
VIVEK BOSE <161508048+bravo2024@users.noreply.github.com>
2025-08-21 03:10:48 +0530

    Update classify.py

--- classify.py
+++ classify.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import mean_absolute_error, r2_score, accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import mean_absolute_error, r2_score, accuracy_score, classification_report, confusion_matrix, log_loss
 import matplotlib.ticker as mtick
 from amadeus import Client
 import os
